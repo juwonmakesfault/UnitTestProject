e8fbfaac4813c548ae9239dbb28d7155ffa2e55b
clearlyhunch <edgecoding@naver.com>
2019-05-07 16:22:55 +0900

    Add insertTest

--- src/main/java/Service/GradeService.java
+++ src/main/java/Service/GradeService.java
@@ -1,8 +1,9 @@
 package Service;
 
 import Domain.Grade;
+import Repository.GradeRepository;
 
 public class GradeService {
 
-
+    private GradeRepository gradeRepository;
 }
--- src/test/java/Service/GradeServiceTest.java
+++ src/test/java/Service/GradeServiceTest.java
@@ -1,17 +1,39 @@
 package Service;
 
 import Domain.Grade;
+import Repository.GradeRepository;
 import org.junit.*;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
-class GradeServiceTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.spy;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GradeServiceTest {
+
+    @Spy
+    private GradeRepository gradeRepository;
 
     @InjectMocks
     private GradeService gradeService;
 
     @Mock
     Grade grade;
+
+    @Test
+    public void insertOnceTest(){
+        grade.setGrade("A0");
+        gradeRepository.insertGrade(grade);
+        List<Grade> grades =  gradeRepository.getGrades();
+        assertThat(grades.size(),is(1));
+    }
 
 
 
